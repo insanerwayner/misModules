46c1bd68740af005db9ca46e4fecbf7874db7a26
Wayne Reeves <wreeves@lifepathsystems.org>
2020-10-01 14:28:33 -0500

    changed the misExchange module

--- misExchange/misExchange.psm1
+++ misExchange/misExchange.psm1
@@ -1,6 +1,2 @@
-if ( $ExchangeSession -eq $null -and ( test-connection misexch01 -count 1 -ErrorAction SilentlyContinue ) )
-	{
-	$cred = Get-XMLPassword -Name Exchange -Type Credential 
-	$ExchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://misexch01/PowerShell/ -Authentication Kerberos -Credential $Cred
-	Import-PSSession $ExchangeSession -DisableNameChecking | Out-Null
-	}
+# requires -Modules ExchangeOnlineManagement
+Connect-ExchangeOnline -UserPrincipalName "$($env:username)@lifepathsystems.org" -ShowProgress $True
